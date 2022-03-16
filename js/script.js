06b6c167939271f54b80ebfa2edcc7c7c5159b3c
Shadman Ahsan <88704323+Shadman-2021@users.noreply.github.com>
2022-03-16 18:38:49 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,47 @@
 // Smooth scrolling
 
 $('a[href*="#"]').click(function(e) {
-  e.preventDefault();
-  var target = this.hash;
-  $target = $(target);
+    e.preventDefault();
+    var target = this.hash;
+    $target = $(target);
 
-  $('html, body').stop().animate({
-    'scrollTop': $target.offset().top
-  }, 900, 'swing', function () {
-    window.location.hash = target;
-  });
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 900, 'swing', function() {
+        window.location.hash = target;
+    });
+});
+
+//Toggle button 
+$(function() {
+    $('#toggle-icon').click(function() {
+        if ($("#menu").is(":visible")) {
+            $('#menu').hide();
+
+            $('.profession-id').show();
+            $('#introduction').show();
+            $('#portfolio').show();
+            $('#contact').show();
+            $('#projects').show();
+            $('#achivements').show();
+            $('#contact').show();
+            $('#intro').show();
+            $('#footer').show();
+
+        } else {
+            $('#menu').show();
+
+            $('.profession-id').hide();
+            $('#introduction').show();
+            $('#portfolio').show();
+            $('#contact').show();
+            $('#projects').hide();
+            $('#achivements').hide();
+            $('#contact').hide();
+            $('#intro').hide();
+            $('#footer').hide();
+        }
+
+        return false;
+    });
 });
