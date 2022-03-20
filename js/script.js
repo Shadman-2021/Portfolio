5fd974f8ce69a0042f978b4c144a098644728e48
Shadman Ahsan <88704323+Shadman-2021@users.noreply.github.com>
2022-03-20 06:45:26 +0600

    update

--- js/script.js
+++ js/script.js
@@ -13,37 +13,19 @@ $('a[href*="#"]').click(function(e) {
 });
 
 //Toggle button 
-$(function() {
-    $('#toggle-icon').click(function() {
-        if ($("#menu").is(":visible")) {
-            $('#menu').hide();
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-            $('.profession-id').show();
-            $('#introduction').show();
-            $('#portfolio').show();
-            $('#contact').show();
-            $('#projects').show();
-            $('#achivements').show();
-            $('#contact').show();
-            $('#intro').show();
-            $('#footer').show();
-
-        } else {
-            $('#menu').show();
-
-            $('.profession-id').hide();
-            $('#introduction').show();
-            $('#portfolio').show();
-            $('#contact').show();
-            $('#projects').hide();
-            $('#achivements').hide();
-            $('#contact').hide();
-            $('#intro').hide();
-            $('#footer').hide();
-        }
-
-        return false;
+hamburger.addEventListener('click', () => {
+    //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
     });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
 });
 
 // carosal
